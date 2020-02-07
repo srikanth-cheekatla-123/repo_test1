a52b01c3eb780df41da8814c14034ad59e4f491c
srikanth-cheekatla-123 <60467305+srikanth-cheekatla-123@users.noreply.github.com>
2020-02-07 16:49:33 +0530

    Create final.rb

---
+++ final.rb
@@ -0,0 +1,28 @@
+$arr= ["hello","w234","wassup","23333",1 ,"ab12"]
+$arr= ["hello","w234","wassup","23333",1 ,"ab12"]
+#puts (" #{$arr[5].class}")
+print ("#{$arr}\n")
+class Strings
+    def Imp()
+        @arr2 = Array.new
+        @arr3 = Array.new
+        @arr4 = Array.new
+        $arr.each do |x|
+            if x.class == Fixnum
+                    @arr2.push(x)
+                elsif x.class == String    
+                    @arr3.push(x)
+                else 
+                    @arr4.push(x)
+             end
+			if x.class == Fixnum
+                    @arr2.push(x)
+                elsif x.class == String    
+                    @arr3.push(x)
+                else 
+                    @arr4.push(x)
+             end    
+        end   
+     end
+   end
+end
