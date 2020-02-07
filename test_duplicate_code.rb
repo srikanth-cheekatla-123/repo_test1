c832297feb242338f5392518fba4937263acbbcf
srikanth-cheekatla-123 <60467305+srikanth-cheekatla-123@users.noreply.github.com>
2020-02-07 15:35:50 +0530

    Create test_duplicate_code.rb

---
+++ test_duplicate_code.rb
@@ -0,0 +1,29 @@
+$arr= ["hello","w234","wassup","23333",1 ,"ab12"]
+#puts (" #{$arr[5].class}")
+print ("#{$arr}\n")
+class Strings
+    def Imp()
+        @arr2 = Array.new
+        @arr3 = Array.new
+        @arr4 = Array.new
+        $arr.each do |x|
+            if x.class == Fixnum
+                    @arr2.push(x)
+                elsif x.class == String    
+                    @arr3.push(x)
+                else 
+                    @arr4.push(x)
+            end    
+        end
+        $arr.each do |x|
+            if x.class == Fixnum
+                    @arr2.push(x)
+                elsif x.class == String    
+                    @arr3.push(x)
+                else 
+                    @arr4.push(x)
+            end    
+        end
+    end
+    
+end
